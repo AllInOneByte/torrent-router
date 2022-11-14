8da2e136a3c16a3b928cb3768214e6588ed67075
Alejandro Pereira <alexpereiracarballo007@gmail.com>
2022-11-14 18:36:54 +0100

    Changed Solid to React

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,34 @@
-import type { Component } from "solid-js";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
 
-import logo from "./logo.svg";
-import styles from "./App.module.css";
+function App() {
+  const [count, setCount] = useState(0)
 
-const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-      </header>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
