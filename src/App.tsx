7a4f779676ad81114c4e57d8cd461b2e40df537d
Alejandro Pereira <alexpereiracarballo007@gmail.com>
2022-11-15 23:24:36 +0100

    Added Chakra UI as component library

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { ProgressBar } from "react-step-progress-bar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        position: "absolute",
+        margin: 0,
+        padding: 0,
+        border: 0,
+      }}
+    >
+      Hello
+      <ProgressBar
+        percent={75}
+        filledBackground="linear-gradient(to right, #fefb72, #f0bb31)"
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
